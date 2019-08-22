f59a80fdb0cfa22f37a0dfc5637d18d019460978
Nicolas Eduardo Ramirez <nramirez@mcti.local>
2019-08-22 14:22:23 -0300

    adding filter features

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RoomContext } from '../Context';
+import Title from '../components/Title';
 
 export default function RoomsFilter() {
-    return(
-        <div>room filter</div>
+    const context = useContext(RoomContext);
+    const {
+        handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets
+    } = context;
+    return (
+        <section className="filter-container">
+            <Title title="search rooms"/>
+            <form className="filter-form">
+               {/*select type*/}
+               <div className="form-group">
+                   <label htmlFor="type">room type</label>
+                   <select name="" id=""></select>
+               </div>
+               {/*end select type */} 
+            </form>
+        </section>
     );
 }
